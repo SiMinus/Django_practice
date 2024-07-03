87e5f5320e7700a89c2c7318f1a13459cc46513e
崔宸玮 <cuichenwei@M2Pro.local>
2024-07-03 02:59:19 +0100

    step4

---
+++ step4/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function CreateRoomPage(props) {
+    return (
+        <div>
+            This is the create room page
+        </div>
+    );
+}
+export default CreateRoomPage;
---
+++ step4/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function RoomJoinPage(props) {
+    return (
+        <div>
+            This is the room join page
+        </div>
+    );
+}
+
+export default RoomJoinPage;
+
+
