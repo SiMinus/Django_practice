78c3a254fd2022ca4b6afbfa7bdbb985172cf66d
崔宸玮 <cuichenwei@M2Pro.local>
2024-07-04 16:30:19 +0100

    step8

---
+++ step8/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('home', views.RoomView.as_view()),
+    path('create-room', views.CreateRoomView.as_view()),
+    path('get-room', views.GetRoom.as_view()),
+    path('join-room', views.JoinRoom.as_view()),
+]
