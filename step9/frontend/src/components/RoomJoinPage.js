7282d72641d9e14bf0244af3ab88e0c9275a6ea2
崔宸玮 <cuichenwei@M2Pro.local>
2024-07-05 02:27:17 +0100

    Update HomePage with new buttons and codeChecking

---
+++ step9/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { TextField, Button, Grid, Typography } from "@mui/material";
+import { Link, useNavigate} from "react-router-dom";
+
+function RoomJoinPage(props) {
+
+   const navigate = useNavigate();
+  //  const [state, setState] = useState({
+  //   roomCode: "",
+  //   error: ""
+  //  })
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+
+   const handelTextFieldChange = (e) => {
+    setRoomCode(e.target.value);
+   } 
+
+   const handleJoinButton = () => {
+     const requestOptions = {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: roomCode
+      })
+     };
+     fetch("/api/join-room", requestOptions)
+       .then((response) => {
+        if(response.ok) {
+          navigate(`/room/${roomCode}`);
+        } else {
+          setError("Room not found.");
+        }
+       })
+       .catch((error) => {
+        console.log(error);
+       })
+
+
+   }
+
+
+    return (
+        <Grid container spacing={1} >
+          <Grid item xs={12} align="center">
+            <Typography variant="h4" component="h4">
+              Join A Room
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <TextField 
+              error={error}
+              label="code"
+              placeholder="Enter A Room Code"
+              value={roomCode}
+              helperText={error}
+              variant="outlined"
+              onChange={handelTextFieldChange}
+            />
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleJoinButton}
+            >
+              Enter A Room
+            </Button>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Button
+              color="secondary"
+              variant="contained"
+              to="/"
+              component={Link}
+            >
+              Back
+            </Button>
+          </Grid>
+        </Grid>
+    );
+}
+
+export default RoomJoinPage;
+
+
