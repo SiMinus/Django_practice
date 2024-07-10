338ea2a1f499078cfe52f515bed8e0c0a033315f
崔宸玮 <cuichenwei@M2Pro.local>
2024-07-10 04:32:10 +0100

    Music Player

---
+++ step14/frontend/src/components/MusicPlayer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+    Grid,
+    Typography,
+    Card,
+    IconButton,
+    LinearProgress
+} from '@mui/material'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import PauseIcon from '@mui/icons-material/Pause'
+import SkipNextIcon from '@mui/icons-material/SkipNext'
+
+
+function MusicPlayer (props) {
+
+    const { title, artist, image_url, duration, time, is_playing } = props.song;
+    
+    const songProgress = (time / duration) * 100;
+
+    return (
+        <Card style={{ padding: '16px', margin: '16px', maxWidth: '400px', height: '150px' }}>
+            <Grid container spacing={1} alignItems="center">
+                <Grid item xs={4} align="center">
+                    <img src={image_url} alt="Album Cover" style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
+                </Grid>
+                <Grid item xs={8} align="center">
+                    <Typography variant="h5" component="h5">
+                        {title}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        {artist}
+                    </Typography>
+                    <div >
+                        <IconButton>
+                            {is_playing ? <PauseIcon /> : <PlayArrowIcon />}
+                        </IconButton>
+                        <IconButton>
+                            <SkipNextIcon />
+                        </IconButton>
+                    </div>
+                </Grid>
+            </Grid>
+            <LinearProgress variant="determinate" value={songProgress} style={{ marginTop: '16px' }} />
+        </Card>
+    );
+
+
+
+}
+
+export default MusicPlayer;
---
+++ step14/spotify/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, redirect
+from django.conf import settings
+from api.models import Room
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from requests import Request, post
+from .util import update_or_create_user_tokens, is_spotify_authenticated, execute_spotify_request
+
+class AuthURL(APIView):
+    def get(self, request, format=None):
+        force_auth = request.GET.get('force_auth', 'false').lower() == 'true'
+        scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+
+        url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'scope': scopes,
+            'response_type': 'code',
+            'redirect_uri': settings.REDIRECT_URI,
+            'client_id': settings.CLIENT_ID
+        }).prepare().url
+
+        if force_auth:
+            url += "&show_dialog=true"
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
+    
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': settings.REDIRECT_URI,
+        'client_id': settings.CLIENT_ID,
+        'client_secret': settings.CLIENT_SECRET
+    }).json()
+
+    access_token = response.get('access_token')
+    refresh_token = response.get('refresh_token')
+    token_type = response.get('token_type')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(request.session.session_key, access_token, refresh_token, token_type, expires_in)
+
+    return redirect('frontend:')
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(self.request.session.session_key)
+        return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+    
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)
+        if room.exists():
+            room = room[0]
+        else:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+        host = room.host
+        endpoint = 'player/currently-playing'
+        response = execute_spotify_request(host, endpoint)
+        
+        if 'error' in response or 'item' not in response:
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        item = response.get('item')
+        duration = item.get('duration_ms')
+        progress = response.get('progress_ms')
+        album_cover = item.get('album').get('images')[0].get('url')
+        is_playing = response.get('is_playing')
+        song_id = item.get('id')
+
+        artist_string = ""
+
+        for i, artist in enumerate(item.get('artists')):
+            if i > 0:
+                artist_string += ", "
+            name = artist.get('name')
+            artist_string += name
+
+        song = {
+            'title': item.get('name'),
+            'artist': artist_string,
+            'duration': duration,
+            'time': progress,
+            'image_url': album_cover,
+            'is_playing': is_playing,
+            'votes': 0,
+            'id': song_id
+        }
+
+        return Response(song, status=status.HTTP_200_OK)
